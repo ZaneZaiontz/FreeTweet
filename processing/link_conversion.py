475f6f157799a0a259edb488b4130117d2ed0b27
Zane <zanezaiontz@live.com>
2022-12-24 20:12:37 -0600

    inital push

---
+++ processing/link_conversion.py
@@ -0,0 +1,60 @@
+""" This code converts twitter links to add fx and remove extra unneeded info """
+import os, sys
+
+IS_SPOILER: bool = True
+SPOILER_GAP: int = 5 # default value of 5
+TWIT_PROXY: str = "fx"
+
+def read_file() -> list:
+    """Reads in links from file"""
+    f = open("links.txt", "r")
+    fixed_links = [fix_links(x).strip('\n') for x in f]
+    return fixed_links
+
+
+def fix_links(user_input) -> str:
+    """ 
+    This function takes links and strips out the garbage 
+    
+    int link_gap: Position in the string to add TWIT_PROXY to, should be placed right after 'https://'
+    
+    """
+    link_gap: int = 8
+    x = user_input.rfind('?')
+    
+    if x != -1:
+        return user_input[0:link_gap] + TWIT_PROXY + user_input[link_gap:x]
+    else:
+        return user_input[0:link_gap] + TWIT_PROXY + user_input[link_gap:]
+    
+def add_spoiler(links) -> list:
+    """Add a discord spoiler tag to the given links"""
+    new_links = []
+    for index, value in enumerate(links):
+        if index % SPOILER_GAP == 0:
+            new_links.append("|| " + value + " ||")
+        else:
+            new_links.append(value)
+    return new_links
+
+
+def print_links(links) -> None:
+    print()
+    for index, value in enumerate(links):
+        if (index + 1) % 5 == 0:
+            print(f"{value}\n")
+        else:
+            print(value)
+    print()
+
+        
+def main():
+    """Main entry to link _conversion.py"""
+    links = read_file()
+    if IS_SPOILER:
+        links = add_spoiler(links)
+    print_links(links)
+
+
+if __name__ == "__main__":
+    main()
